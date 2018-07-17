fcf44db2203e1fbbd31e4adc4a79651331128310
Stepan <40896580+StepashkaFB@users.noreply.github.com>
2018-07-17 16:56:26 +0300

    Add files via upload

---
+++ Task3/Task3/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+	class Employee
+	{
+		string FullName;
+		public string _FullName
+		{
+			get { return FullName; }
+			set { FullName = value; }
+		}
+		int Salary;
+		public int _Salary
+		{
+			get { return Salary; }
+			set { Salary = value; }
+		}
+		public Employee(string Name, int Salary)
+		{
+			this.FullName = Name;
+			this.Salary = Salary;
+		}
+		public static int SortBySalary(Employee E1, Employee E2)
+		{
+			return E1._Salary.CompareTo(E2._Salary);
+		}
+	}
+}
+
---
+++ Task3/Task3/_Main.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+	class _Main
+	{
+		static void AddNewEmpl(ref List<Employee> lstEmpl)//Creat new Employee
+		{
+			Console.Write("Enter Full Name:  ");
+			string Fullname = Console.ReadLine();
+			Console.Write("Enter the salary:  ");
+			int Salary = int.Parse(Console.ReadLine());
+			lstEmpl.Add(new Employee(Fullname, Salary));
+		}
+		static void BeforeSort(ref List<Employee> lstEmpl)//Show full List
+		{
+			Console.WriteLine("Before sort:");
+			foreach (Employee empl in lstEmpl)//Before sort
+			{
+				Console.WriteLine("Worker: {0} -- {1}", empl._FullName, empl._Salary);
+			}
+		}
+		static void ResultOfSort(ref List<Employee> lstEmpl)//Sort by salary
+		{
+			lstEmpl.Sort(Employee.SortBySalary);
+			Console.WriteLine();
+			Console.WriteLine("After sort:");
+			foreach (Employee empl in lstEmpl)//After sort
+			{
+				Console.WriteLine("Worker: {0} -- {1}", empl._FullName, empl._Salary);
+			}
+		}
+		static void Main(string[] args)
+		{
+			List<Employee> lstEmpl = new List<Employee>();//initialization
+			lstEmpl.Add(new Employee("Stepan HH GG", 2900));
+			lstEmpl.Add(new Employee("Ivan JJ LL", 4040));
+			lstEmpl.Add(new Employee("Alex II OO", 3500));
+			lstEmpl.Add(new Employee("Dominik PP YY", 7340));
+			lstEmpl.Add(new Employee("Anton FF UU", 5688));
+			lstEmpl.Add(new Employee("Valantin WW EE", 5566));
+			lstEmpl.Add(new Employee("Vlad DD FF", 7745));
+
+			Console.WriteLine();
+			bool Continue = true;
+			Console.WriteLine("Select the following actions:");
+			Console.WriteLine("1 - Show List");
+			Console.WriteLine("2 - Sort List and Show List");
+			Console.WriteLine("3 - Add new Employee");
+			while (Continue)
+			{
+				int choise = int.Parse(Console.ReadLine());
+				switch (choise)
+				{
+					case 1:
+						BeforeSort(ref lstEmpl);
+						break;
+					case 2:
+						ResultOfSort(ref lstEmpl);
+						break;
+					case 3:
+						AddNewEmpl(ref lstEmpl); 
+						break;
+					default:
+						Console.WriteLine("Incorrect selection");
+						break;
+				}
+				Console.WriteLine("Continue? Enter y/n");
+				string Choise = Console.ReadLine();
+				if (Choise == "n")
+					break;
+				else
+					Console.WriteLine("Select another actions");
+				continue;
+			}
+		}
+	}
+}
