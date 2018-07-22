e8e58e6412d80496c692ea5f107a8ebe97500f35
Stepan <40896580+StepashkaFB@users.noreply.github.com>
2018-07-22 23:03:42 +0300

    Add files via upload

---
+++ TASK4/TASK4/Predators.cs
@@ -0,0 +1,11 @@
+﻿namespace TASK4
+{
+    class Predators : IAnimalInZoo, IColor
+    {
+        public string Name { get; set; }
+        public bool Fly { get; set; }
+        public bool Walk { get; set; }
+        public string Color { get; set; }
+    }
+}
+
---
+++ TASK4/TASK4/_Main.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TASK4
+{
+    class _Main
+    {
+        static void Main(string[] args)
+        {
+            //Create a group for storage
+            Group<Bird> BirdGroup = new Group<Bird>();
+
+            Bird Duck = new Bird() { Name = "Duck",Color = "Black", Fly = true, Walk = true };
+            Bird Chiken = new Bird() { Name = "Chiken", Color = "Red", Fly = false, Walk = true };
+            Bird Parrot = new Bird() { Name = "Parrot", Color = "Green", Fly = true, Walk = true };
+            //Addition to the group of animals
+            BirdGroup.GroupAnimal(Duck);
+            BirdGroup.GroupAnimal(Chiken);
+            BirdGroup.GroupAnimal(Parrot);
+            
+            Group<Predators> PredatorGroup = new Group<Predators>();
+            Group<Cargo> CargoGroup = new Group<Cargo>();
+            Group<Feline> FelineGroup = new Group<Feline>();
+            Group<Primates> PrimatesGroup = new Group<Primates>();
+            
+        }
+    }
+}
