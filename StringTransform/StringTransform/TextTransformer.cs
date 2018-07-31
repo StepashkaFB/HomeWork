cab1c23eb38c1ae00d4b3eccad4cd2be816e90a0
Stepan <40896580+StepashkaFB@users.noreply.github.com>
2018-07-31 20:18:11 +0300

    Add files via upload

---
+++ StringTransform/StringTransform/TextTransformer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+
+namespace StringTransform
+{
+    class TextTransformer : IEnumerable
+    {
+        public TextTransformer(int Amount)
+        {
+            tempstring = new string[Amount];
+        }
+        public string[] tempstring;
+
+        public IEnumerator GetEnumerator()
+        {
+            for (int Counter = 0; Counter < tempstring.Length; Counter++)
+            {
+                if (tempstring[Counter] == null || tempstring[Counter] == "")
+                {
+                    yield return tempstring[Counter] = "valuable";
+                }
+                else
+                {
+                    yield return tempstring[Counter].ToUpper();
+                }
+            }
+        }
+    }
+}
---
+++ StringTransform/StringTransform/_Main.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace StringTransform
+{
+    class _Main
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter an amount of strings:  ");
+            int Amount = int.Parse(Console.ReadLine());
+            TextTransformer transform = new TextTransformer(Amount);
+
+            Console.WriteLine("Filling strings:");
+            for (int Counter = 0; Counter < Amount; Counter++)//input
+            {
+                transform.tempstring[Counter] = Console.ReadLine();
+            }
+
+            foreach(string Name in transform)//output
+            {
+                Console.WriteLine(Name);
+            }
+        }
+    }  
+}
