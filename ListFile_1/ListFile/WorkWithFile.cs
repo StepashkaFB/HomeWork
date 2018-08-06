3b9c5f4f36f0de6f7f630490c0a9450c0010bbf5
Stepan <40896580+StepashkaFB@users.noreply.github.com>
2018-08-06 13:36:17 +0300

    Add files via upload

---
+++ ListFile_1/ListFile/WorkWithFile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ListFile
+{
+    class WorkWithFile
+    {
+        // show list of files
+        public static void ShowFiles(List<string> FileStorage)
+        {
+            int temp = 0;
+            foreach (var File in FileStorage)
+            {
+                Console.WriteLine("Number of file -- {0} -- Name: {1}", temp++, File);
+            }
+        }
+        //read contents of file
+        public static void ReadSelectedFile(List<string> FileStorage, int number)
+        {
+            StreamReader streader = new StreamReader(FileStorage[number]);
+            Console.WriteLine(streader.ReadToEnd());
+            streader.Close();
+        }
+    }
+}
---
+++ ListFile_1/ListFile/_Main.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ListFile
+{
+    class _Main
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter a path, like --  D:\\Qwerty");
+                string path = Console.ReadLine();
+                //list all files in directory
+                List<string> FileStorage = new List<string>(Directory.GetFiles(path));
+
+                WorkWithFile.ShowFiles(FileStorage);
+
+                Console.Write("Enter a number of file you want to read: ");
+                int number = int.Parse(Console.ReadLine());
+                WorkWithFile.ReadSelectedFile(FileStorage, number);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
