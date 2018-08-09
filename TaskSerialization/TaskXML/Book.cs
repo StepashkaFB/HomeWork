411da5c609af2c013092d365f8b7155ae45fe9ad
Stepan <40896580+StepashkaFB@users.noreply.github.com>
2018-08-09 21:57:36 +0300

    Add files via upload

---
+++ TaskSerialization/TaskXML/Book.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Json;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace TaskXML
+{
+    [DataContract][Serializable]
+    public class Book
+    {
+        [DataMember][XmlAttribute]
+        string FullName;
+        
+        public string _FullName
+        {
+            get { return FullName; }
+            set { FullName = value; }
+        }
+        [DataMember][XmlAttribute]
+        string Author;
+        
+        public string _Author
+        {
+            get { return Author; }
+            set { Author = value; }
+        }
+        [DataMember][XmlAttribute]
+        int AmountPage;
+        
+        public int _AmountPage
+        {
+            get { return AmountPage; }
+            set { AmountPage = value; }
+        }
+        public Book() { }
+        public Book(string FullName, string Author, int AmountPage)
+        {
+            this.FullName = FullName;
+            this.Author = Author;
+            this.AmountPage = AmountPage;
+        }
+
+        //XML serialization
+        public void SaveToXml(string path, Book tempbook)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Book));
+            using (StreamWriter strW = new StreamWriter(path))
+            {
+                serializer.Serialize(strW, tempbook);
+            }
+        }
+        public string ShowFullBook()
+        {
+            return "FullName: " + FullName + "--" +
+                " Author: " + Author + "--" +
+                " Page: " + AmountPage;
+        }
+        public void LoadFromXML(string path, ref Book tepmbook)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Book));
+            using (StreamReader streader = new StreamReader(path))
+            {
+                tepmbook = (Book)serializer.Deserialize(streader);
+            }
+        }
+        //Show XML File
+        public void ShowXML(string path)
+        {
+            using (StreamReader streader = new StreamReader(path))
+            {
+                Console.WriteLine(streader.ReadToEnd());
+                streader.Close();
+            }
+        }
+        //JSON serialization
+        public void SaveToJson(string path, ref Book tempBook)
+        {
+            
+            using (FileStream filest = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                DataContractJsonSerializer jsonFormatter = new DataContractJsonSerializer(typeof(Book));
+                jsonFormatter.WriteObject(filest, tempBook);
+            }
+        }
+        public void LoadFromJSON(string path, ref Book tempBook)
+        {
+            using (FileStream filest = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                DataContractJsonSerializer jsonFormatter = new DataContractJsonSerializer(typeof(Book));
+                tempBook = (Book)jsonFormatter.ReadObject(filest);
+            }
+        }
+        //Using binary serialization
+        public void SaveToBinary(string path, ref Book tempbook)
+        {
+            IFormatter formatter = new BinaryFormatter();
+            using (Stream stream = new FileStream(path, FileMode.Create))
+            {
+                formatter.Serialize(stream, tempbook);
+            }
+        }
+        public void LoadFromBinary(string path, ref Book tempbook)
+        {
+            IFormatter formatter = new BinaryFormatter();
+            using (Stream stream = new FileStream(path, FileMode.Open))
+            {
+                tempbook = (Book)formatter.Deserialize(stream);
+            }
+        }
+        //Using the DataContractSerializer
+        public void SaveToDataSerialization(string path, ref Book tempbook)
+        {
+            using (FileStream filest = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                DataContractSerializer DataSer = new DataContractSerializer(typeof(Book));
+                DataSer.WriteObject(filest, tempbook);
+            }
+        }
+        public void LoadFromDataSerialization(string path, ref Book tempbook)
+        {
+            using (FileStream filest = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                DataContractSerializer DataSer = new DataContractSerializer(typeof(Book));
+                tempbook = (Book)DataSer.ReadObject(filest);
+            }
+        }
+    }
+}
---
+++ TaskSerialization/TaskXML/_Main.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace TaskXML
+{
+    class _Main
+    {
+        static void Main(string[] args)
+        {
+            //Demonstration Save to XML file
+            Book book_1 = new Book("Name!", "Author11", 35);
+            string path = @"D:\папка\TaskXML\TaskXML\XMLFile11.xml";
+            book_1.SaveToXml(path, book_1);
+            book_1.ShowXML(path);
+
+            Book book_2 = new Book("","",0);
+            //Load From XML
+            book_2.LoadFromXML(path, ref book_2);
+            Console.WriteLine(book_2.ShowFullBook());
+
+            string path1 = @"D:\папка\TaskXML\TaskXML\BookJson.json";
+            //Save to JSON file
+            book_2.SaveToJson(path1, ref book_2);
+            Book book_3 = new Book("", "", 0);
+
+            //Load From JSON File
+            book_3.LoadFromJSON(path1, ref book_3);
+            Console.WriteLine(book_3.ShowFullBook());
+
+            //Binary
+            string path2 = @"D:\папка\TaskXML\TaskXML\BinaryData.bin";
+            Book NameBook = new Book("GoodName", "GoodAuthor", 50);
+            NameBook.SaveToBinary(path2, ref NameBook);
+
+            Book book_4 = new Book("", "", 0);
+            book_4.LoadFromBinary(path2, ref book_4);
+            Console.WriteLine(book_4.ShowFullBook());
+
+            //DataSer
+            string path3 = @"D:\папка\TaskXML\TaskXML\DataSer.xml";
+            Book SimpleBook = new Book("WithOutName", "WithOutAuthor", 500);
+            SimpleBook.SaveToDataSerialization(path3, ref SimpleBook);
+
+            Book book_5 = new Book("", "", 0);
+            book_5.LoadFromDataSerialization(path3, ref book_5);
+            Console.WriteLine(book_5.ShowFullBook());
+        }
+    }
+}
+
