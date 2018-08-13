49b457c4360da421f85c457b189c59289faa14b4
Stepan <40896580+StepashkaFB@users.noreply.github.com>
2018-08-13 21:02:30 +0300

    Add files via upload

---
+++ TaksEncrypt/TaksEncrypt/Encryption.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace TaksEncrypt
+{
+    class Encryption
+    {
+        public static string PrivateKey { get; set; }
+        public static string PublicKey { get; set; }
+        public static UnicodeEncoding Encoder = new UnicodeEncoding();
+
+        public static string Encrypt(string Data)
+        {
+            RSACryptoServiceProvider RSA = new RSACryptoServiceProvider();
+            RSA.FromXmlString(PublicKey);
+
+            byte[] DataToEncrypt = Encoder.GetBytes(Data);
+            byte[] EncryptedByteArray = RSA.Encrypt(DataToEncrypt, false).ToArray();
+
+            int length = EncryptedByteArray.Count();
+            int item = 0;
+            StringBuilder stringbild = new StringBuilder();
+
+            foreach (Byte temp in EncryptedByteArray)
+            {
+                item++;
+                stringbild.Append(temp);
+                if (item < length)
+                {
+                    stringbild.Append("|");
+                }   
+            }
+            return stringbild.ToString();
+        }
+        public static string Decrypt(string Data)
+        {
+            RSACryptoServiceProvider RSA = new RSACryptoServiceProvider();
+            string[] DataArray = Data.Split(new char[] { '|' });
+            byte[] DataByte = new byte[DataArray.Length];
+
+            //Convert to Byte
+            for (int Counter = 0; Counter < DataArray.Length; Counter++)
+            {
+                DataByte[Counter] = Convert.ToByte(DataArray[Counter]);
+            }
+
+            RSA.FromXmlString(PrivateKey);
+            byte[] decryptedByte = RSA.Decrypt(DataByte, false);
+            return Encoder.GetString(decryptedByte);
+        }
+    }
+}
---
+++ TaksEncrypt/TaksEncrypt/_Main.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace TaksEncrypt
+{
+    class _Main
+    {
+
+        static void Main(string[] args)
+        {
+            RSACryptoServiceProvider RSA = new RSACryptoServiceProvider();
+            Encryption.PrivateKey = RSA.ToXmlString(true);
+            Encryption.PublicKey = RSA.ToXmlString(false);
+
+            Console.WriteLine("Enter your Text: ");
+            string MainText = Console.ReadLine();
+            Console.WriteLine("RSA -- Text before encryption: " + MainText);
+
+            string EncryptedText = Encryption.Encrypt(MainText);
+            Console.WriteLine("RSA -- Encrypted Text: \n" + EncryptedText);
+
+            string DecipheredText = Encryption.Decrypt(EncryptedText);
+            Console.WriteLine("RSA -- Decrypted Text: " + DecipheredText);
+        }
+    }
+}
