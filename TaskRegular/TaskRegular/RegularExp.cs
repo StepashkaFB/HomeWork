72add6a99c3c6514d3b18c639a8404931e79498b
Stepan <40896580+StepashkaFB@users.noreply.github.com>
2018-08-13 16:02:52 +0300

    Add files via upload

---
+++ TaskRegular/TaskRegular/RegularExp.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace TaskRegular
+{
+    class RegularExp
+    {
+        public static bool EmailAdress(string InputtedEmail)
+        {
+            const string Expresion = "\\w+([-+.']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*";
+            if (Regex.IsMatch(InputtedEmail, Expresion))
+            {
+                if (Regex.Replace(InputtedEmail, Expresion, string.Empty).Length == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool Url(string InputtedURL)
+        {
+            const string Expresion = @"^http(s)?://([\w-]+.)+[\w-]+(/[\w- ./?%&=])?$";
+            if (Regex.IsMatch(InputtedURL, Expresion))
+            {
+                if (Regex.Replace(InputtedURL, Expresion, string.Empty).Length == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+        //Another Way
+        public static bool URLValid(string InputtedURL)
+        {
+            Uri uriResult;
+            bool result = Uri.TryCreate(InputtedURL, UriKind.Absolute, out uriResult)
+                && uriResult.Scheme == Uri.UriSchemeHttp;
+            return result;
+        }
+        public static bool FilePathValid(string InputtedPath)
+        {
+            const string Expresion = @"((?:[a-zA-Z]\:){0,1}(?:[\\/][\w.]+){1,})";
+            if (Regex.IsMatch(InputtedPath, Expresion))
+            {
+                if (Regex.Replace(InputtedPath, Expresion, string.Empty).Length == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ TaskRegular/TaskRegular/_Main.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TaskRegular
+{
+    class _Main
+    {
+        static void Main(string[] args)
+        {
+            //True
+            Console.WriteLine(RegularExp.EmailAdress("stepan_kill@mail.ru"));
+            //False
+            Console.WriteLine(RegularExp.EmailAdress("stepan_kill@mailru"));
+            //True
+            Console.WriteLine(RegularExp.Url("http://www.google.by"));
+            Console.WriteLine(RegularExp.URLValid("http://www.google.by"));
+            //True
+            Console.WriteLine(RegularExp.FilePathValid(@"D:\папка\dd"));
+            //False
+            Console.WriteLine(RegularExp.FilePathValid(@"D:\папка\:dd"));
+        } 
+    }
+}
