19005acfe2c1c88eaaf49f17f8a88992cab15ee3
Stepan <40896580+StepashkaFB@users.noreply.github.com>
2018-08-14 17:05:12 +0300

    Add files via upload

---
+++ TaskGCD/TaskGCD/GettingGCD.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+
+namespace TaskGCD
+{
+    class GettingGCD
+    {
+        public static int GCD(params int[] temp)
+        {
+            int min = temp.Min(), OurGCD;
+            for (OurGCD = min; OurGCD > 0; OurGCD--)
+            {
+                bool reminderIsZero = true;
+                for (int Counter = 0; Counter < temp.Length; Counter++)
+                {
+                    reminderIsZero &= temp[Counter] % OurGCD == 0;
+
+                }
+                if (reminderIsZero) break;
+            }
+            return OurGCD;
+        }
+        public static int FindGCD(int first, int second, int third)
+        {
+            int GCD = Math.Min(first, Math.Min(second, third));
+            for (; GCD > 1; GCD--)
+            {
+                if (first % GCD == 0 && second % GCD == 0 && third % GCD == 0)
+                    break;
+            }
+            return GCD;
+        }
+        public static int GetGCD(int value1, int value2)
+        {
+            int TempF = 1;
+            int TempS = value1 < value2 ? value1 : value2;
+            int TempT = 0;
+            int OurGCD = value1 < value2 ? value2 : value1;
+            do
+            {
+                while (OurGCD - (TempF * TempS) >= TempS)
+                {
+                    TempF++;
+                }
+                TempT = OurGCD - (TempF * TempS);
+                OurGCD = TempS;
+                TempS = TempT;
+            }
+            while (TempT > 0);
+            return OurGCD;
+        }
+    }
+}
---
+++ TaskGCD/TaskGCD/_Main.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace TaskGCD
+{
+    class _Main
+    {
+        static void Main(string[] args)
+        {
+            int[] IntArray = new int[3] {36, 220, 48 };
+            int First = 36;
+            int Second = 220;
+            int Third = 48;
+
+            Stopwatch stopW = new Stopwatch();
+            int OurGCD = 0;
+            stopW.Start();
+            OurGCD = GettingGCD.GCD(IntArray);
+            stopW.Stop();
+            Console.WriteLine(OurGCD);
+            Console.WriteLine(stopW.Elapsed);
+
+            stopW.Reset();
+            stopW.Start();
+            OurGCD = GettingGCD.FindGCD(First, Second, Third);
+            stopW.Stop();
+            Console.WriteLine(OurGCD);
+            Console.WriteLine(stopW.Elapsed);
+
+            stopW.Reset();
+            stopW.Start();
+            OurGCD = GettingGCD.GetGCD(GettingGCD.GetGCD(First, Second), Third);
+            stopW.Stop();
+            Console.WriteLine(OurGCD);
+            Console.WriteLine(stopW.Elapsed);
+        } 
+    }
+}
