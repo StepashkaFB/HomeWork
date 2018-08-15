c6a4997c8e32333252c14c54799ceb83be7216a2
Stepan <40896580+StepashkaFB@users.noreply.github.com>
2018-08-15 17:50:24 +0300

    Add files via upload

---
+++ TaskStopWatch/TaskStopWatch/STOPWATCH.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading;
+
+namespace TaskStopWatch
+{
+    class STOPWATCH
+    {
+        static bool Control = true;
+        static int Counter = 0;
+
+        public static void  CountingTime()
+        {
+            while(Control)
+            {
+                Counter++;
+                Thread.Sleep(1);
+            }
+        }
+        public static void Start()
+        {
+            Control = true;
+        }
+        public static void Stop()
+        {
+            Control = false;
+        }
+        public static void Reset()
+        {
+            Counter = 0;
+        }
+        public static string Show()
+        {
+            return Counter.ToString() + "ms";
+        }
+    }
+}
---
+++ TaskStopWatch/TaskStopWatch/_Main.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+
+namespace TaskStopWatch
+{
+    class _Main
+    {
+        public static int FirstAlg(params int[] temp)
+        {
+            int min = temp.Min(), OurGCD;
+            for (OurGCD = min; OurGCD > 0; OurGCD--)
+            {
+                bool reminderIsZero = true;
+                for (int Counter = 0; Counter < temp.Length; Counter++)
+                {
+                    reminderIsZero &= temp[Counter] % OurGCD == 0;
+
+                }
+                if (reminderIsZero) break;
+            }
+            return OurGCD;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] TempArray = new int[3];
+            Random rand = new Random();
+            for (int i = 0; i < TempArray.Length; i++)
+            {
+                TempArray[i] = rand.Next();
+            }
+                
+            Thread thread_SW = new Thread(new ThreadStart(STOPWATCH.CountingTime));
+            thread_SW.IsBackground = true;
+            thread_SW.Start();
+            Console.WriteLine(FirstAlg(TempArray));
+            STOPWATCH.Stop();
+            thread_SW.Join();
+            Console.WriteLine(STOPWATCH.Show());
+
+            //Checking, Fault is less than 0.01%
+            Stopwatch StopW = new Stopwatch();
+            StopW.Start();
+            FirstAlg(TempArray);
+            StopW.Stop();
+            Console.WriteLine(FirstAlg(TempArray));
+            Console.WriteLine(StopW.ElapsedMilliseconds + "ms");
+        }
+    }
+}
