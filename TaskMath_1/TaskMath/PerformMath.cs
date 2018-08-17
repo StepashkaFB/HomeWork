055d3ac71846ab28b02158217b0447bb364539ed
Stepan <40896580+StepashkaFB@users.noreply.github.com>
2018-08-17 15:00:28 +0300

    Add files via upload

---
+++ TaskMath_1/TaskMath/PerformMath.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+
+namespace TaskMath
+{
+    class PerformMath
+    {
+        public static void FillingSet(ref double[] Set)
+        {
+            for (int Counter = 0; Counter < Set.Length; Counter++)
+            {
+                Set[Counter] = double.Parse(Console.ReadLine());
+            }
+        }
+        public static void FillingSetRandom(ref double[] Set)
+        {
+            Random rand = new Random();
+            for (int Counter = 0; Counter < Set.Length; Counter++)
+            {
+                Set[Counter] = rand.NextDouble();
+            }
+        }
+        public static void ShowSet(ref double[] Set)
+        {
+            for (int Counter = 0; Counter < Set.Length; Counter++)
+            {
+                Console.Write(Set[Counter] + " ");
+            }
+            Console.WriteLine();
+        }
+        //performing math statistics operations
+        public static double RangeOfVariation(double[] Set)
+        {
+            var min = Set.Min();
+            var max = Set.Max();
+            return max - min;
+        }
+        public static double ExpectedValue( double[] Set)
+        {
+            double sum = 0;
+            for (int Counter = 0; Counter < Set.Length; Counter++)
+            {
+                sum += Set[Counter];
+            }  
+            return sum/Set.Length;
+        }
+        public static double MeanDeviation(double[] Set)
+        {
+            double sum = 0;
+            double ExpValue = ExpectedValue(Set);
+            for (int Counter = 0; Counter < Set.Length; Counter++)
+            {
+                sum += Math.Abs((Set[Counter] - ExpValue));
+            }
+            sum /= Set.Length;
+            return sum;
+        }
+        public static double Dispersion(double[] Set)
+        {
+            double sum = 0;
+            double ExpValue = ExpectedValue(Set);
+            for (int Counter = 0; Counter < Set.Length; Counter++)
+            {
+                sum += Math.Pow(Set[Counter] - ExpValue, 2);
+            }
+            sum /= Set.Length;
+            return sum;
+        }
+        public static double StandardDeviation(double[] Set)
+        { 
+            return Math.Sqrt(Dispersion(Set));
+        }
+    }
+}
+
---
+++ TaskMath_1/TaskMath/_Main.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace TaskMath
+{
+    class _Main
+    {
+        static void Main(string[] args)
+        {
+            double[] Set = new double[] { };
+            bool Continue = true;
+            //Text menu
+            Console.WriteLine("Select the following actions:");
+            Console.WriteLine("1 - Creat a Set");
+            Console.WriteLine("2 - Fill the Set");
+            Console.WriteLine("3 - Fill the Set (Random)");
+            Console.WriteLine("4 - Show the Set");
+            Console.WriteLine("5 - Show Expected Value");
+            Console.WriteLine("6 - Show Dispersion");
+            Console.WriteLine("7 - Show Standard Deviation");
+            Console.WriteLine("8 - Show Mean linear deviation");
+            Console.WriteLine("9 - Show the range of variation");
+            while (Continue)
+            {
+                int choise = int.Parse(Console.ReadLine());
+                switch (choise)
+                {
+                    case 1:
+                        Console.Write("Enter the number of elements: ");
+                        int amount = int.Parse(Console.ReadLine());
+                        Set = new double[amount];
+                        break;
+                    case 2:
+                        PerformMath.FillingSet(ref Set);
+                        break;
+                    case 3:
+                        PerformMath.FillingSetRandom(ref Set);
+                        break;
+                    case 4:
+                        PerformMath.ShowSet(ref Set);
+                        break;
+                    case 5:
+                        Console.WriteLine(PerformMath.ExpectedValue(Set));
+                        break;
+                    case 6:
+                        Console.WriteLine(PerformMath.Dispersion(Set));
+                        break;
+                    case 7:
+                        Console.WriteLine(PerformMath.StandardDeviation(Set));
+                        break;
+                    case 8:
+                        Console.WriteLine(PerformMath.MeanDeviation(Set));
+                        break;
+                    case 9:
+                        Console.WriteLine(PerformMath.RangeOfVariation(Set));
+                        break;
+                    default:
+                        Console.WriteLine("Incorrect selection");
+                        break;
+                }
+                Console.WriteLine("Continue? Enter y/n");
+                string Choise = Console.ReadLine();
+                if (Choise == "n")
+                    break;
+                else
+                    Console.WriteLine("Select another actions");
+                continue;
+            }
+        }
+    }
+}
+
